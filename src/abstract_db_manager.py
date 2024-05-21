fd570ed27684ab9d3d10af2a0909b5b97495f891
Julya Shapaeva <shapaeva03@mail.ru>
2024-05-21 18:37:09 +0300

    Fix annotations in abstract methods

--- src/abstract_db_manager.py
+++ src/abstract_db_manager.py
@@ -10,7 +10,7 @@ class AbstractDBManager(ABC):
         pass
 
     @abstractmethod
-    def create_tables(self):
+    def create_tables(self) -> None:
         """Создает таблицы в базе данных"""
         pass
 
@@ -20,27 +20,27 @@ class AbstractDBManager(ABC):
         pass
 
     @abstractmethod
-    def get_companies_and_vacancies_count(self):
+    def get_companies_and_vacancies_count(self) -> list[dict]:
         """Получает список всех компаний и количество вакансий у каждой компании"""
         pass
 
     @abstractmethod
-    def get_all_vacancies(self):
+    def get_all_vacancies(self) -> list[tuple]:
         """Получает список всех вакансий с указанием названия компании,
         названия вакансии и зарплаты и ссылки на вакансию"""
         pass
 
     @abstractmethod
-    def get_avg_salary(self):
+    def get_avg_salary(self) -> int:
         """Получает среднюю зарплату по вакансиям"""
         pass
 
     @abstractmethod
-    def get_vacancies_with_higher_salary(self):
+    def get_vacancies_with_higher_salary(self) -> list[tuple]:
         """Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям"""
         pass
 
     @abstractmethod
-    def get_vacancies_with_keyword(self, keyword: str):
+    def get_vacancies_with_keyword(self, keyword: str) -> list[tuple]:
         """Получает список всех вакансий, в названии которых содержатся переданные в метод слова"""
         pass
